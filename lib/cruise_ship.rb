bd9e2a864d385cea45b2224ba676ed8ce27d90a5
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-11 20:34:10 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,3 +8,11 @@
 
 
   # add the code snippet here!
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+  end
+  
+end
